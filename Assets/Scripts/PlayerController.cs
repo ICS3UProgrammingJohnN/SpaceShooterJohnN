38f368d3ab30b6b8d839e699744b5d572a60af52
John Ngundeng <36854092+JohnNgundeng@users.noreply.github.com>
2018-11-21 16:53:06 -0500

    er455

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        private Rigidbody;
+        void Start()
+        {
+
+            rb = GetComponent<Rigidbody>();
+        }
+    }
+}
